4e2282d82d8c74001fed59647c1a8752bd1ca20b
wangyuchun <78609144@qq.com>
2017-09-06 10:23:38 +0800

    init project with maven

---
+++ src/main/java/com/risetek/usbprinter/App.java
@@ -0,0 +1,13 @@
+package com.risetek.usbprinter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
